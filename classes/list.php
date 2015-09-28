c29b657979ec92ee34889eb3223123d0358576ce
Ken McGonigal <ken.mcgonigal@gmail.com>
2015-09-28 01:29:59 -0700

    still working on classes/list.php

---
+++ classes/list.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+	*list component
+*/
+
+class Mylist extends OssnObject {
+		/** Initialize Attributes;
+		*
+		* return void;
+		*/
+		private function initializeAttributes() {
+				$this->annotation = new OssnAnnotations;
